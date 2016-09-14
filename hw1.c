e4c51988debbe38c92c72188a0383d99a83d5525
ric1234 <richard.noronha@rediffmail.com>
2016-09-14 10:43:07 -0600

    All files added

---
+++ hw1.c
@@ -0,0 +1,21 @@
+#include "hw1.h"
+char reverse(char * str, int length)
+{
+	char revs[30];
+	int var;
+	if(length>30)		//string is too long
+		return '1';
+	else if(str=="\0")	//Null string
+		return '1';
+	else
+		{	var= length;			//A variable used in reversing
+			for(int i=0; i<length; i++)	//Code for reversing
+			{
+				revs[i]=str[var-1];
+				var-=1;
+			}
+			//revs[length]="\0";		//NULL character at the end
+			printf("\n %s \n", revs);		//print the string
+			return '0';	
+		}
+} 
